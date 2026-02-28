98e947677fe0a63a657e7dac24b00a6d37394cfb
egoistic-era <princeverma6734@gmail.com>
2026-02-28 22:43:15 +0530

    if_else_if_demo.c

---
+++ if_else_if_demo.c
@@ -0,0 +1,30 @@
+// Program to demonstrate if-else-if statement
+// Author: Prince Verma
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num > 0)
+    {
+        printf("\nThe number is Positive.\n");
+    }
+    else if (num < 0)
+    {
+        printf("\nThe number is Negative.\n");
+    }
+    else
+    {
+        printf("\nThe number is Zero.\n");
+    }
+
+    return 0;
+}
