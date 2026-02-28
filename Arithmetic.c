a1013d53b734aab97cfd5dcf8a23175a7e82fbf6
egoistic-era <princeverma6734@gmail.com>
2026-02-28 18:13:38 +0530

    Arithmetic.c

--- Arithmetic.c
+++ Arithmetic.c
@@ -1,3 +1,34 @@
 # egoistic-era
 # arithmetic-Operations 
 Author:- Prince Verma
+
+// Arithmetic Operations Program
+// Author: Prince Verma
+
+#include <stdio.h>
+
+int main() {
+
+    int a, b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    printf("\nResults:\n");
+
+    printf("Addition: %d\n", a + b);
+    printf("Subtraction: %d\n", a - b);
+    printf("Multiplication: %d\n", a * b);
+
+    if (b != 0) {
+        printf("Division: %d\n", a / b);
+        printf("Modulus: %d\n", a % b);
+    } else {
+        printf("Division and Modulus not possible (division by zero).\n");
+    }
+
+    return 0;
+}
