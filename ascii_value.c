732bb7e150b5f295eea82df561bb8e513d3cb877
egoistic-era <princeverma6734@gmail.com>
2026-02-28 22:28:58 +0530

    ascii_value.c

---
+++ ascii_value.c
@@ -0,0 +1,20 @@
+// Program to find ASCII value of a character
+// Author: Prince Verma
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+
+    char ch;
+
+    printf("Enter a character: ");
+    scanf(" %c", &ch);
+
+    printf("\nCharacter entered: %c\n", ch);
+    printf("ASCII value: %d\n", ch);
+
+    return 0;
+}
